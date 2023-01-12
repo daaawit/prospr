1161d1624cf8a8679f58bdfe3e4c9797dec59d08
David Schischke <hi@davidschischke.com>
2023-01-12 08:55:59 +0100

    docs: added docstrings and error messages to models/models.py

--- models/models.py
+++ models/models.py
@@ -5,9 +5,23 @@ from models import resnet18, resnet20, resnet50
 
 
 def model_factory(config: str, dataset: str, disable_auto_patch: bool) -> nn.Module:
+    """Load the correct model based on the user inputs.
+
+    Args:
+        config (str): The model to use. Choose from ["resnet18", "resnet20", "resnet50", "vgg19", "vgg16"].
+        dataset (str): The dataset to use. Choose from ["cifar10", "cifar100", "tiny_imagenet", "imagenet"].
+        disable_auto_patch (bool): If True, disables automatic patching of ResNet and VGG models to work 
+        with input sizes smaller than ImageNet
+    Raises:
+        ValueError: If an unknown dataset is passed to dataset.
+        TypeError: If an unknown model is passed to config.
+
+    Returns:
+        nn.Module: An instance of the correct model.
+    """
 
     dataset = dataset.lower()
-    if dataset == "cifar10":
+    if dataset == "cifar10": # TODO: If we have this, why do we need num_classes for DataLoaders?
         num_classes = 10
     elif dataset == "cifar100":
         num_classes = 100
@@ -16,7 +30,7 @@ def model_factory(config: str, dataset: str, disable_auto_patch: bool) -> nn.Mod
     elif dataset == "imagenet":
         num_classes = 1000
     else:
-        raise ValueError
+        raise ValueError(f"Unknown dataset {dataset}")
 
     if disable_auto_patch or (dataset == "imagenet"):
         patch_for_smaller_input = False
@@ -28,7 +42,7 @@ def model_factory(config: str, dataset: str, disable_auto_patch: bool) -> nn.Mod
             num_classes=num_classes, patch_for_smaller_input=patch_for_smaller_input
         )
     elif config == "resnet20":
-        assert dataset == "cifar10"
+        assert dataset == "cifar10", "Model must be resnet20 when using cifar10"
         model = resnet20()
     elif config == "resnet50":
         model = resnet50(
@@ -55,6 +69,6 @@ def model_factory(config: str, dataset: str, disable_auto_patch: bool) -> nn.Mod
             model.avgpool = nn.AdaptiveAvgPool2d((1, 1))
             model.classifier = nn.Linear(512, num_classes)
     else:
-        raise TypeError
+        raise TypeError(f"Uknown model {config} passed to config")
 
     return model
